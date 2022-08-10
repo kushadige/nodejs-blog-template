2e073a73829251a784f80c63e2d71edc256cb6c6
blitzkrieg <oguzhankuslar@gmail.com>
2022-08-11 00:06:30 +0300

    Nodejs blog template

---
+++ data.js
@@ -0,0 +1,16 @@
+module.exports.pages = [
+    {
+        title: 'Home',
+        description: 'Venenatis urna cursus eget nunc scelerisque. Convallis tellus id interdum velit laoreet id donec ultrices. Ut lectus arcu bibendum at varius vel pharetra. Sagittis purus sit amet volutpat consequat mauris. Pharetra convallis posuere morbi leo urna molestie at elementum. Aliquet nibh praesent tristique magna sit amet purus gravida. Diam in arcu cursus euismod quis viverra nibh cras pulvinar. Viverra vitae congue eu consequat ac felis donec et. Ultrices eros in cursus turpis massa tincidunt. Ut ornare lectus sit amet est placerat.'
+    },
+    {
+        title: 'About',
+        description: 'A scelerisque purus semper eget duis at. Eget sit amet tellus cras adipiscing. Sagittis nisl rhoncus mattis rhoncus. In pellentesque massa placerat duis ultricies lacus sed turpis tincidunt. Eget velit aliquet sagittis id consectetur purus ut faucibus. Nisl nisi scelerisque eu ultrices vitae auctor. Elit sed vulputate mi sit amet mauris commodo. Aenean sed adipiscing diam donec adipiscing tristique risus. Purus faucibus ornare suspendisse sed. Ut placerat orci nulla pellentesque dignissim enim sit.'
+    },
+    {
+        title: 'Contact',
+        description: 'Leo integer malesuada nunc vel. Lectus sit amet est placerat in egestas erat imperdiet sed. Augue ut lectus arcu bibendum at. Aliquet lectus proin nibh nisl condimentum id venenatis a. Mauris a diam maecenas sed enim ut. Sed arcu non odio euismod lacinia at quis risus sed. Faucibus in ornare quam viverra orci sagittis eu. Purus viverra accumsan in nisl. Odio facilisis mauris sit amet massa vitae tortor. Dignissim sodales ut eu sem integer vitae justo eget. Cursus sit amet dictum sit amet justo donec enim diam. Pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna. Non diam phasellus vestibulum lorem sed. Sem viverra aliquet eget sit amet. Gravida rutrum quisque non tellus orci ac auctor augue.'
+    }
+];
+
+module.exports.posts = [];
